7b449c04165d2f856fbe914172a5b2576ab08899
Lekzy2 <noah.ringsell@elev.ga.ntig.se>
2023-01-11 14:50:48 +0100

    yeah

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
+    return {
+      dir: {
+        input: "views",
+        output: "dist"
+      }
+    }
+  };
+  
